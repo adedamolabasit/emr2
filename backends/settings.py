99b0bba4ea675906d7181bdb75fad03bddd490e8
adedamolabasit <53799680+adedamolabasit@users.noreply.github.com>
2022-05-23 19:23:04 +0100

    ---

--- backends/settings.py
+++ backends/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'emr',
+    'app',
     'rest_framework',
     'corsheaders',
     'storages'
